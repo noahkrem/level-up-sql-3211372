d34d2567b42b65c085d29ef447e68a90e56e3d24
Noah Kremler <72223746+noahkrem@users.noreply.github.com>
2024-05-22 16:57:34 +0000

    Challenges 7 and 8

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,11 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+SELECT 
+  *
+FROM Reservations R
+JOIN Customers C ON C.CustomerID = R.CustomerID
+WHERE C.LastName LIKE 'St%'
+  AND R.PartySize = 4
+ORDER BY R.Date DESC;
