670ca45530bdfba2efad430ff41da76ab8d19ac3
Noah Kremler <72223746+noahkrem@users.noreply.github.com>
2024-05-22 17:55:40 +0000

    All restaurant challenges complete

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,15 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+SELECT
+  C.FirstName,
+  C.LastName,
+  C.Email,
+  O.CustomerID,
+  COUNT(O.OrderID) OrderCount
+FROM Orders O
+JOIN Customers C ON O.CustomerID = C.CustomerID
+GROUP BY O.CustomerID
+ORDER BY OrderCount DESC
+LIMIT 20;
