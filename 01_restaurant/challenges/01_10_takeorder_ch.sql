6a20ee1c4188148b9b393677f99e8aec3add2a14
Noah Kremler <72223746+noahkrem@users.noreply.github.com>
2024-05-22 17:44:07 +0000

    Challenges 10 and 11

--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -7,3 +7,33 @@
 -- 1 Tropical Blue Smoothie
 -- Delivery date and time: September 20, 2022 @ 2PM (14:00)
 -- There are no taxes or other fees.
+
+
+INSERT INTO Orders
+  (CustomerID, OrderDate)
+VALUES (
+  (SELECT 
+    CustomerID
+  FROM Customers
+  WHERE FirstName = 'Loretta'
+    AND LastName = 'Hundey'),
+  '2022-09-20 ' );
+
+SELECT *
+FROM Orders O
+JOIN Customers C ON C.CustomerID = O.CustomerID
+WHERE FirstName = 'Loretta'
+  AND LastName = 'Hundey';
+
+INSERT INTO OrdersDishes 
+  (OrderID, DishID)
+VALUES
+  (1001, (SELECT DishID FROM Dishes WHERE Name = 'House Salad')),
+  (1001, (SELECT DishID FROM Dishes WHERE Name = 'Mini Cheeseburgers')),
+  (1001, (SELECT DishID FROM Dishes WHERE Name = 'Tropical Blue Smoothie'));
+
+SELECT
+  SUM(D.Price)
+FROM Dishes D
+JOIN OrdersDishes OD ON OD.DishID = D.DishID
+WHERE OD.OrderID = 1001;
--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,25 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+SELECT * FROM Customers
+WHERE FirstName = 'Cleo'
+  AND LastName = 'Goldwater';
+-- Customer ID is 42
+
+SELECT * FROM Dishes
+WHERE Name = 'Quinoa Salmon Salad';
+-- Dish ID is 9
+
+UPDATE CustomersDishes
+SET DishID = 9
+WHERE CustomerID = 42;
+
+UPDATE Customers
+SET FavoriteDish = (SELECT DishID FROM Dishes WHERE Name = 'Quinoa Salmon Salad')
+WHERE CustomerID = 42;
+
+SELECT * FROM Customers
+JOIN Dishes ON Customers.FavoriteDish = Dishes.DishID;
+
+  
