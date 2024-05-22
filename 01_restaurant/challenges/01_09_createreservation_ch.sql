687fb8d05ea1943be1d8759745eb6a8fb9803f6b
Noah Kremler <72223746+noahkrem@users.noreply.github.com>
2024-05-22 17:08:57 +0000

    Challenge 9

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,29 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+INSERT INTO Customers
+  (FirstName, LastName, Email)
+VALUES (
+  'Sam',
+  'McAdams',
+  'smac@kinetecoinc.com' );
+
+SELECT * FROM Customers
+WHERE Email = 'smac@kinetecoinc.com';
+
+INSERT INTO Reservations
+  (CustomerID, Date, PartySize)
+VALUES (
+  ( SELECT
+      CustomerID
+    FROM Customers
+    WHERE Email = 'smac@kinetecoinc.com'
+      AND FirstName = 'Sam'
+      AND LastName = 'McAdams' ),
+  '2022-08-12 18:00:00',
+  5 );
+
+SELECT * FROM Reservations
+WHERE CustomerID = 102;
+
